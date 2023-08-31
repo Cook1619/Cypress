2fee1609695d61a2d712e8e5f6ae23e96b648bfb
Matt <matt@Matts-MBP.home>
2023-08-31 15:26:30 -0500

    Adding tests around modal functionality

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,13 +1,24 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 
 describe('tasks page', () => {
-  it('should render main image', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:5173/')
+  })
+  it('should render main image', () => {
     cy.get('.main-header img')
   })
 
   it('should have title', () => {
-    cy.visit('http://localhost:5173/');
+
+    cy.get('h1').should('have.length', 1)
     cy.get('h1').contains('React Tasks')
+  })
+
+  it('should open and close the modal by clicking the appropriate buttons', () => {
+    cy.get('[data-cy=start-add-task-button]').click();
+    cy.get('.modal').should('be.visible')
+    cy.get('button[type="button"]').contains('Cancel').click()
+    cy.get('.backdrop').should('not.exist')
+    cy.get('.modal').should('not.exist')
   })
 })
