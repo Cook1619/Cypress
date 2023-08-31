e13a6aa32f15a47c75f35cb7e4506ab8e27c1fa6
Matt <matt@Matts-MBP.home>
2023-08-31 15:41:20 -0500

    Adding tests for adding a tasks

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -15,10 +15,23 @@ describe('tasks page', () => {
   })
 
   it('should open and close the modal by clicking the appropriate buttons', () => {
-    cy.get('[data-cy=start-add-task-button]').click();
+    cy.get('[data-cy=start-add-task-button]').click()
     cy.get('.modal').should('be.visible')
     cy.get('button[type="button"]').contains('Cancel').click()
     cy.get('.backdrop').should('not.exist')
     cy.get('.modal').should('not.exist')
+  })
+
+  it('should create a task', () => {
+    cy.get('[data-cy=start-add-task-button]').click()
+    cy.get('input[id="title"]').type('Walk Dog')
+    cy.get('textarea[id="summary"]').type('I need to walk my dog at least 3 miles')
+    cy.get('select[id="category"]').select('important')
+    cy.get('button[type="submit"]').click()
+    cy.get('.backdrop').should('not.exist')
+    cy.get('.modal').should('not.exist')
+    cy.get('h2').contains('Walk Dog')
+    cy.get('p').contains('I need to walk my dog at least 3 miles')
+    cy.get('.task-list').should('have.length', 1)
   })
 })
